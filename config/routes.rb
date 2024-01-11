f021c187bdab51b59116e38e7dd93fdb1a27b14b
alissongrilo <alisson.grilo@gooxxy.com>
2024-01-11 19:18:21 -0300

    feat: create backoffice folders for profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
   devise_for :users
   devise_for :admins
   get 'welcome/index'
